b76de60f7ba67ac4cb8a777c401dbaaa8058bce2
ItsDropper <hugohakonen@gmail.com>
2026-07-22 21:50:38 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  token: process.env.DISCORD_TOKEN,
-  clientId: process.env.CLIENT_ID,
-  guildId: process.env.GUILD_ID,
-  ticketCategoryId: process.env.TICKET_CATEGORY_ID || '1529393937171746918',
-  supportRoleId: process.env.SUPPORT_ROLE_ID || null,
-};
